0f827a96571f8589242d71e2a39519a5cced311e
Markus Reinhold <markusreinhold@icloud.com>
2020-09-11 22:35:24 +0200

    Rename RefererHeaderAgainstAllowedOriginsGuard #8
    To RefererHeaderMatchesAllowedOriginsGuard.

--- src/Guard/RefererHeaderMatchesAllowedOriginsGuard.php
+++ src/Guard/RefererHeaderMatchesAllowedOriginsGuard.php
@@ -5,7 +5,7 @@ namespace Marein\StandardHeadersCsrfBundle\Guard;
 
 use Symfony\Component\HttpFoundation\Request;
 
-final class RefererHeaderAgainstAllowedOriginsGuard implements Guard
+final class RefererHeaderMatchesAllowedOriginsGuard implements Guard
 {
     /**
      * @var string[]
